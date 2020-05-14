688201d300afc0b7062ea83e5454029556523089
Gerard Roche <gerardroche@users.noreply.github.com>
2020-05-14 17:57:25 +0100

    Fix skipped PostgreSQL tests on Travis CI
    The PostgreSQL tests were being skipped due to a failure to connect to
    the test database.

--- travis/setup-mysql.sh
+++ travis/setup-mysql.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
-if [ -n "$ARM64" ]; then
-  sudo mysql -e 'CREATE USER "travis"@"localhost" IDENTIFIED BY ""'
-  sudo mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "travis"@"localhost"'
-fi
-mysql -e "CREATE DATABASE IF NOT EXISTS test"
+#!/bin/sh
+set -ev
+
+# if test -n "$ARM64"; then
+#     mysql -vvv -e 'CREATE USER "travis"@"localhost" IDENTIFIED BY ""'
+#     mysql -vvv -e 'GRANT ALL PRIVILEGES ON *.* TO "travis"@"localhost"'
+# fi
+
+mysql -vvv -e "CREATE DATABASE IF NOT EXISTS test"
--- travis/setup-pgsql.sh
+++ travis/setup-pgsql.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-echo '
-<?php $conn_str .= " user=postgres"; ?>' >> "./ext/pgsql/tests/config.inc"
-if [ -z "$ARM64" -o -z "$S390X"]; then
-  psql -c 'create database test;' -U postgres
+#!/bin/sh
+set -ev
+
+if test -z "$ARM64$S390X"; then
+    psql -c "ALTER USER postgres PASSWORD 'postgres';" -U postgres
+    psql -c "create database test;" -U postgres
 fi
