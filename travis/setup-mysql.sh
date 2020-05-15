bf5c213ddaffc8c5a5d007b050f91207511e88f2
Gerard Roche <gerardroche@users.noreply.github.com>
2020-05-15 12:23:50 +0100

    :metal:

--- travis/setup-mysql.sh
+++ travis/setup-mysql.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -ev
 
-# if test -n "$ARM64"; then
-#     mysql -vvv -e 'CREATE USER "travis"@"localhost" IDENTIFIED BY ""'
-#     mysql -vvv -e 'GRANT ALL PRIVILEGES ON *.* TO "travis"@"localhost"'
-# fi
+if test -n "$ARM64"; then
+    mysql -vvv -e 'CREATE USER "travis"@"localhost" IDENTIFIED BY ""'
+    mysql -vvv -e 'GRANT ALL PRIVILEGES ON *.* TO "travis"@"localhost"'
+fi
 
 mysql -vvv -e "CREATE DATABASE IF NOT EXISTS test"
