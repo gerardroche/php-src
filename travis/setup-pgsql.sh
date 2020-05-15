4959a68f944050be35ff8b6e44496e730c3b229c
Gerard Roche <gerardroche@users.noreply.github.com>
2020-05-15 14:00:04 +0100

    :metal:

--- travis/setup-pgsql.sh
+++ travis/setup-pgsql.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ev
 
-if test -z "$ARM64$S390X"; then
-    psql -c "ALTER USER postgres PASSWORD 'postgres';" -U postgres
-    psql -c "create database test;" -U postgres
-fi
+# if test -z "$ARM64$S390X"; then
+psql -c "ALTER USER postgres PASSWORD 'postgres';" -U postgres
+psql -c "create database test;" -U postgres
+# fi
